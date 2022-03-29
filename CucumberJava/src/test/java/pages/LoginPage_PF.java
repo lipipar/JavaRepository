af88454a23f1370a8dc3f228f5362ed2dbb1a993
Lipi Parikh <lparikh@greatelk.com>
2022-03-29 16:51:40 +1300

    git push insert
    git push
    
    \\

--- CucumberJava/src/test/java/pages/LoginPage_PF.java
+++ CucumberJava/src/test/java/pages/LoginPage_PF.java
@@ -3,8 +3,10 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage_PF {
+	
 	
 	@FindBy(id="name")
 	WebElement txt_Username;
@@ -17,10 +19,25 @@ public class LoginPage_PF {
 	
 	WebDriver driver;
 	
-	public LoginPage_PF()
+	public LoginPage_PF(WebDriver driver)
 	{
-		
+		this.driver= driver;
+		PageFactory.initElements(driver, this);
+		//this refers to the current class you could either put LoginPage_PF.class
 	}
 	
-
+  public void enterUserName(String username)
+  {
+	  txt_Username.sendKeys(username);
+  }
+  
+  public void enterPassword(String password)
+  {
+	  txt_Password.sendKeys(password);
+  }
+  
+  public void clickonLogin()
+  {
+	  btn_login.click();
+  }
 }
--- SeleniumLipi/src/basicweb/WebElementTest.java
+++ SeleniumLipi/src/basicweb/WebElementTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.ui.Select;
 
 public class WebElementTest {
 
@@ -30,22 +31,46 @@ public class WebElementTest {
 //				driver.quit();
 		
 		//Testing other Webelements
-//		String url = "https://trytestingthis.netlify.app/";
-//		driver.get(url);
-//		List<WebElement> options = driver.findElements(By.name("Optionwithcheck[]"));
-//		
-//		for(WebElement element : options)
-//		{
-//			System.out.println(element.getText());
-//		}
-//		WebElement csselement = driver.findElement(By.cssSelector("#fname"));
-//		csselement.sendKeys("lipi");
+		String url = "https://trytestingthis.netlify.app/";
+		driver.get(url);
+		List<WebElement> options = driver.findElements(By.name("Optionwithcheck[]"));
+		
+		for(WebElement element : options)
+		{
+			System.out.println(element.getText());
+		}
+		WebElement csselement = driver.findElement(By.cssSelector("#fname"));
+		csselement.sendKeys("lipi");
+		
+		WebElement radiobtn = driver.findElement(By.xpath("//input[@id='male']"));
+		
+		
+		if (radiobtn.isSelected()== false) {
+			radiobtn.click();
+            System.out.println("RadioButton is Toggled On");					
+
+        } 
+		
+		
+		WebElement selectbox = driver.findElement(By.id("option"));
+		Select select = new Select(selectbox);
+		select.selectByValue("option 2");
+		
+		WebElement checkbox1 = driver.findElement(By.id("moption"));
+		if (checkbox1.isSelected()== false) {
+			checkbox1.click();
+            System.out.println("Checkbox is Toggled On");					
+
+        } 
+		
+		
+		
 		
 		//To use Relative locators
-		driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
-		WebElement loginbutton = driver.findElement(By.id("btnLogin"));
-		WebElement bttn = driver.findElement(RelativeLocator.with(By.tagName("input")).above(loginbutton));
-		bttn.sendKeys("ABCD");
+//		driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+//		WebElement loginbutton = driver.findElement(By.id("btnLogin"));
+//		WebElement bttn = driver.findElement(RelativeLocator.with(By.tagName("input")).above(loginbutton));
+//		bttn.sendKeys("ABCD");
 		
 		Thread.sleep(3000);
 		driver.close();
